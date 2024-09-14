4d0eeaa89c4d46b765905cf206f0f8fef5285f67
Dhruv Saxena <saxena25dhruv@gmail.com>
2024-09-14 14:21:16 +0530

    app.py updated

--- app.py
+++ app.py
@@ -1,27 +1,9 @@
-from flask import Flask, render_template, jsonify, request
-from dotenv import load_dotenv
-import os
-from flipkart.retrieval_generation import generation, get_session_history
+from flask import Flask, render_template, request
+from flipkart.retrieval_generation import generation
 from flipkart.data_ingestion import data_ingestion
-from langchain_core.chat_history import BaseChatMessageHistory
-from langchain_community.chat_message_histories import ChatMessageHistory
-from langchain_groq import ChatGroq
-
-
-os.environ["GROQ_API_KEY"]= os.getenv("GROQ_API_KEY")
-
-model = ChatGroq(model="llama-3.1-70b-versatile", temperature=0.5)
-
-chat_history= []
-store = {}
-def get_session_history(session_id: str)-> BaseChatMessageHistory:
-  if session_id not in store:
-    store[session_id]= ChatMessageHistory()
-  return store[session_id]
 
 
 vstore = data_ingestion("done")
-
 chain = generation(vstore)
 
 
